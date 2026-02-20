632b19d41bfa66239e4860d017eec50451b8efd6
evieoz <527eoslislo@frhsd.com>
2026-02-20 10:35:24 -0500

    3

--- script.js
+++ script.js
@@ -1,3 +1,42 @@
+// What the user is currently typing (as text)
+let typedNumberText = ''
+
+// The number we store for calculations
+let storedNumber = null
+
+// The operator currently selected 
+let currentOperator = ''
+
+//Used only for displaying the history line
+// Example: ["3", "+", "4"]
+let historyParts = []
+
+function setStatus(message) {
+    document.getElementById('statusLine').textContent = message
+}
+
+function showSymbol(op) {
+    if (op === '*') return '×';
+    if (op === '/') return '÷';
+    if (op === '-') return '&#x2212';
+    return op;
+}
+
+function updateScreen() {
+    const display = document.getElementById('displayLine')
+    const history = document.getElementById('historyLine')
+    const status = document.getElementById('statusLine')
+
+    display.textContent = typedNumberText 
+}
+
 function pressNumber(digit) {
-    console.log(digit)
+setStatus('')
+    if (typedNumberText === '0') {
+        typedNumberText = digit;
+    } else {
+        typedNumberText = typedNumberText + digit
+    }
+
+    updateScreen()
 }
